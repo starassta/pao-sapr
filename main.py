9e8a6c40ed0bebac035d6bb4e5dece2ec94c0e13
starassta <124889921+starassta@users.noreply.github.com>
2023-03-30 21:19:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-
-# Online Python - IDE, Editor, Compiler, Interprete
-ddd=58
-zig=2
-plotnost=ddd+zig
-print("плотность"+str(plotnost))
-z=3
-z+=1
+I='This is the choice of Steins Gate'
+print (I)
+I_1=I[0]
+print(I_1)
+I_3=I[2]
+print(I_3)
+I_c3=I[-3]
+print(I_c3)
+print(len(I))
+Author="Okabe Reintaro"
+print(Author)
+Author_8=Author[:7]
+print(Author_8)
+Start=int((len(Author))/2)-2
+Finish=int((len(Author))/2)+2
+Author_4=Author[Start:Finish]
+print(Author_4)
+Author_3=Author[::3]
+print(Author_3)
